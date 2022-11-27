b62ae01e282c64855559ca1a5c2805e335d89eb9
Rüdiger Sczuka <ruediger.sczuka@gmail.com>
2022-11-27 22:19:43 +0100

    Umformung von must Precede Formel hinzugefügt

--- Java/src/simulated_annealing/State.java
+++ Java/src/simulated_annealing/State.java
@@ -85,7 +85,7 @@ public class State {
 
     // function checks if order must not be changed
     private static boolean mustPrecede(Customer i, Customer j) {
-        if (i.getId() < j.getId()) {
+        if (i.getId() < j.getId()) {//could be removed as i will always be smaller than j in current implementation
             return i.getOrders().get(0) <= j.getOrders().get(0) + (j.getId() - i.getId() - 1);
         } else
             return false;
